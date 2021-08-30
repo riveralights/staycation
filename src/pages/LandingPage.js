c69371944c496bb57a8fad978d3493dffbc28a56
Rivera Lights <74080651+riveralights@users.noreply.github.com>
2021-08-30 10:56:51 +0700

    Mounting and Styling Most Picked  Components

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
+
 import Header from "parts/Header";
 import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
+
 import LandingPageJSON from "json/landingPage.json";
 
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render() {
     return (
       <>
         {/* Ambil semua props yang ada pada Header */}
         <Header {...this.props} />
-        <Hero data={LandingPageJSON.hero} />
+        <Hero refMostPicked={this.refMostPicked} data={LandingPageJSON.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={LandingPageJSON.mostPicked}
+        />
       </>
     );
   }
---
+++ src/parts/MostPicked.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Button from "elements/Button";
+
+export default function MostPicked(props) {
+  return (
+    <section
+      className="container most-picked"
+      ref={props.refMostPicked}
+      style={{ height: 2000 }}
+    >
+      <h4 className="most-picked-title">Most Picked</h4>
+      <div className="container-grid">
+        {props.data.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className={`item column-4 ${index === 0 ? "row-2" : "row-1"}`}
+            >
+              <div className="card card-featured">
+                <div className="tag">
+                  ${item.price} <span>/ {item.unit}</span>
+                </div>
+                <figure className="img-wrapper">
+                  <img
+                    src={item.imageUrl}
+                    alt={item.name}
+                    className="img-cover"
+                  />
+                </figure>
+                <div className="meta-wrapper">
+                  <Button
+                    className="streched-link d-block text-white"
+                    type="link"
+                    href={`/properties/${item._id}`}
+                  >
+                    <h5>{item.name}</h5>
+                  </Button>
+                  <span>
+                    {item.city}, {item.province}
+                  </span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
