4eddfa71485a80a28b653a6211db29dcf494bdf5
Rivera Lights <74080651+riveralights@users.noreply.github.com>
2021-09-02 09:30:48 +0700

    Slicing Asset and Finishing InputNumber Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./assets/scss/style.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
+import Example from "pages/Example";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route path="/" exact component={LandingPage}></Route>
+        <Route path="/example" component={Example}></Route>
       </Router>
     </div>
   );
